4270e04ace089fcfe7db3737230a2ff33d1ce3ae
TheFatAndFurious <mrguerrilla@gmail.com>
2024-06-03 22:36:47 +0200

    printing the clipboard content

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/go-vgo/robotgo/clipboard"
 	hook "github.com/robotn/gohook"
 )
 
 func main() {
 	hook.Register(hook.KeyDown, []string{"ctrl", "c"}, func(e hook.Event) {
 		fmt.Println("[Event] Ctrl+Shift+X detected!")
+		fmt.Println(clipboard.ReadAll())
 		// hook.End()
 	})
 
